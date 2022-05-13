d39deb47aa773bc9f2541c66f1f410bc109cc264
Kiana Omoerah <kiana.omoerah@gmail.com>
2022-05-13 18:09:25 -0400

    added functional mobile menu

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,24 +1,10 @@
-// html & css: create a burger button and sticky it to the bottom right 
-    // hide our menu by defauly so that it can be summoned by clicking the burger 
-    // in the nav, haev a div with our burger button
-        // font-awesome icons for burger and X to toggle between
-        // media queries to hide it at larger widths
+    const menuButton = document.getElementById('mobileButton');
 
-        // when the user clicks, the menu appears 
+    const mobileMenu = document.querySelector('.mobileMenu');
 
 
-        const menuButton = document.getElementById('mobileButton');
-        const mobileMenu = document.getElementsByClassName('.mobileMenu');
+    menuButton.addEventListener('click', function(e){
 
-        menuButton.addEventListener('click', function(e){
-            // menu slides into view
-            console.log(e);
-            // toggle the display of mobile menu
-            mobileMenu.classList.toggle('menuAppear')
-            
+        mobileMenu.classList.toggle('revealMenu');
 
-
-
-            
-
-        })
+    });
